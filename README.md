4b6e069a09a86ce7bc8a9738ec8c243daa79e661
quintans <paulo.quintans@gmail.com>
2019-01-06 15:02:52 +0000

    updated doc and added grouping to migration

--- README.md
+++ README.md
@@ -5,12 +5,26 @@ DSL for DTO, Service, Entity models to build an enterprise application.
 
 This is only the declaration of the model structure and generation of respective java.
 
-## TODO
-A template project is needed (To be added in the future).
-
+This project aims to provide high level concepts often used when building an enterprise application.
+Then the same concept can be reused to generate (transform) different artifacts (code, text, etc) depending the needs and guidelines (workflows and templates) of a projects.
+These guidelines should be defined elsewhere, usually in the target project.
+You can find an example [here](https://github.com/quintans/mda-model-example) of worflows, models and models all working together.
 
 ## Generating code
 
 Every time we change the `src/main/xsd/*.xsd` files we need to generate the java source.
-We do this with:<br>
-`mvn jaxb2:xjc` 
+We do this with `mvn jaxb2:xjc` 
+
+## Guide
+The following instructions explain how to define the models and how to use resulting data in the templates.
+
+### Transformers
+
+#### FileSystemTransformer
+
+### Entity
+
+
+#### Model
+
+#### Template
--- src/main/java/pt/quintans/mda/enterprise/custom/AllInOneMigrateM2T.java
+++ src/main/java/pt/quintans/mda/enterprise/custom/AllInOneMigrateM2T.java
@@ -1,11 +1,14 @@
 package pt.quintans.mda.enterprise.custom;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import pt.quintans.mda.core.Model;
 import pt.quintans.mda.core.Model2TextAbstract;
+import pt.quintans.mda.core.Tools;
 import pt.quintans.mda.core.Work;
 import pt.quintans.mda.core.WorkerStore;
 import pt.quintans.mda.enterprise.model.entity.Element;
@@ -23,49 +26,68 @@ public class AllInOneMigrateM2T extends Model2TextAbstract {
 		loadMapping(mappings);
 		prepare();
 		
-		Work work =  WorkerStore.get();
-		Map<String, Object> pipeline = (Map<String, Object>) work.getPipeline();
 		Model allModels[] = getFromPipe(PipelineKeys.ALL_MODELS);
 		
+		Work work =  WorkerStore.get();
+		
 		if(allModels.length > 1){
-			List<Entity> migrate = (List<Entity>) pipeline.get(MIGRATE_KEY);
-
-			if(migrate == null){ // not yet transformed
-				migrate = new ArrayList<Entity>();
-				pipeline.put(MIGRATE_KEY, migrate);
-
-				List<Object> newElements = allModels[0].getTransformedObjectList(getStereotype());
-				pipeline.put(NEW_ENTITY_KEY, newElements);
-				List<Object> oldElements = allModels[1].getTransformedObjectList(getStereotype());
-				pipeline.put(OLD_ENTITY_KEY, oldElements);
-				// to changed or to add
-				for(Object obj : newElements){
-					Element element = (Element) obj;
-					Element oldElement = containsElement(element, oldElements);
-					if((element = Entity.differentiate((Entity)oldElement, (Entity)element)) != null){
-						migrate.add((Entity)element);
-					}
+		    String groupby = getOptional(PipelineKeys.GROUP_BY);
+		    if(groupby != null) {
+		    	Map<Object, List<Object>> newElementsMap = Tools.groupBy(allModels[0].getTransformedObjectList(getStereotype()), groupby);
+		    	Map<Object, List<Object>> oldElementsMap = Tools.groupBy(allModels[1].getTransformedObjectList(getStereotype()), groupby);
+				for(Map.Entry<Object, List<Object>> entry : newElementsMap.entrySet()) {
+					putInPipe(PipelineKeys.GROUP, entry.getValue());
+					putInPipe(PipelineKeys.GROUPKEY, entry.getKey());
+					migrate(work, entry.getValue(), oldElementsMap.getOrDefault(entry, Collections.emptyList()));
+					removeFromPipe(PipelineKeys.GROUP);
 				}
-				// to drop
-				for(Object obj : oldElements){
-					Element element = (Element) obj;
-					Element newElement = containsElement(element, newElements);
-					if(newElement == null && (element = Entity.differentiate((Entity)element, (Entity)newElement)) != null){
-						migrate.add((Entity)element);
-					}
-				}
-			}
-
-		    String destinationFile = getDestination();
-			
-		    putInPipe(PipelineKeys.DESTINATION_FILE, destinationFile);
-		    
-		    if(!work.isQuiet())
-		    	System.out.println(String.format("Generating <<%s>> [%s] => %s", getStereotype(), getTemplate(), destinationFile));						
-
-		    dumpToFile(destinationFile);
+			} else {
+				migrate(work, allModels[0].getTransformedObjectList(getStereotype()), allModels[1].getTransformedObjectList(getStereotype()));
+			}			
 		}  
 		
+	}
+	
+	private void migrate(Work work, List<Object> newElements, List<Object> oldElements) {
+		Map<String, Object> pipeline = work.getPipeline();
+		List<Entity> migrate = (List<Entity>) pipeline.get(MIGRATE_KEY);
+
+		if(migrate == null){ // not yet transformed
+			migrate = new ArrayList<Entity>();
+			pipeline.put(MIGRATE_KEY, migrate);
+
+			pipeline.put(NEW_ENTITY_KEY, newElements);
+			pipeline.put(OLD_ENTITY_KEY, oldElements);
+			// to changed or to add
+			for(Object obj : newElements){
+				Element element = (Element) obj;
+				Element oldElement = containsElement(element, oldElements);
+				if((element = Entity.differentiate((Entity)oldElement, (Entity)element)) != null){
+					migrate.add((Entity)element);
+				}
+			}
+			// to drop
+			for(Object obj : oldElements){
+				Element element = (Element) obj;
+				Element newElement = containsElement(element, newElements);
+				if(newElement == null && (element = Entity.differentiate((Entity)element, (Entity)newElement)) != null){
+					migrate.add((Entity)element);
+				}
+			}
+		}
+
+		Map<String, Object> properties = new HashMap<>(pipeline);
+		properties.putAll( getMap());
+		String destinationFile = Tools.applyKeys(getDestination(), properties);
+		destinationFile = Tools.processTemplate(properties, destinationFile);
+		
+	    putInPipe(PipelineKeys.DESTINATION_FILE, destinationFile);
+	    
+	    if(!work.isQuiet())
+	    	System.out.println(String.format("Generating <<%s>> [%s] => %s", getStereotype(), getTemplate(), destinationFile));						
+
+	    dumpToFile(destinationFile);
+
 	}
 
 	private static Element containsElement(Element entity, List<Object> entities){
